ae665aebf7bc88db547a554bf27601683852f0c0
町田千優 <16201chihiro@gmail.com>
2017-11-05 19:51:15 +0900

    Initial Commit

---
+++ NewProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NewProject
+//
+//  Created by 町田千優 on 2017/11/05.
+//  Copyright © 2017年 町田千優. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
